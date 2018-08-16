0498e33623e1fbeed941343df4972a964f840b03
root <sunil1995yadav>
2018-08-17 01:46:58 +0530

    new module

--- custom_qweb_report/models/account_invoice.py
+++ custom_qweb_report/models/account_invoice.py
@@ -16,9 +16,14 @@ class AccountInvoice(models.Model):
             counter=0
             rows=""
             while counter!=(self.company_id.inv_line):
-                rows+="<tr style='border:1px solid black;'>"
+                rows+="<tr style='border:none;'>"
                 for each in range(0,5):
-                    rows+="<td height='20px' style='border:1px solid black'></td>"
+                    if each==0:
+                        rows+="<td height='20px' style='border-top:1px solid black;border-bottom:1px solid black;border-right:1px solid black;border-left:none;'></td>"
+                    elif each==4:
+                        rows+="<td height='20px' style='border-top:1px solid black;border-bottom:1px solid black;border-right:none;border-left:1px solid black;'></td>"
+                    else:
+                        rows+="<td height='20px' style='border:1px solid black'></td>"
                 rows+="</tr>"
                 counter +=1
             return rows
