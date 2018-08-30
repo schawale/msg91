4ab287cceff06f018d51e61ef419010f3b7f44af
root <sunil1995yadav>
2018-08-30 13:43:35 +0530

    report note chnages amount in words changes

--- custom_qweb_report/models/account_invoice.py
+++ custom_qweb_report/models/account_invoice.py
@@ -1,14 +1,13 @@
 from odoo import api, models, fields, _
-from num2words import num2words
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     
     
     def get_num2words_amount(self, amount):
-       amt_word = num2words(amount, lang='en_IN')
-       amt_word = str(amt_word).title()
-       return amt_word + ' Only'    
+       amt_word=self.company_id.currency_id.amount_to_text(float(amount))
+       return amt_word 
+   
     #get extra row in report
     @api.multi
     def get_extra_rows(self,lines):
