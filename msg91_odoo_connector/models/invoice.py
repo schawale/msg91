347ea7d384a6fe32593a1d846f7b06ccc07ed564
kuldeep <kuldeep@localhost>
2018-12-29 10:49:37 +0530

    adding in try catch sms sending code

--- msg91_odoo_connector/models/invoice.py
+++ msg91_odoo_connector/models/invoice.py
@@ -9,22 +9,25 @@ class AccountInvoice(models.Model):
         mobile=False
         api_obj=self.env['api.msg91']
         invoice_result = super(AccountInvoice, self).invoice_validate()
-        sms_template_objs = self.env["sms.template"].sudo().search(
-                [('condition', '=', 'invoice_vaildate')])
-        print ("sms_template_objssms_template_objs",sms_template_objs)
-        for sms_template_obj in sms_template_objs:
-            mobile = self.partner_id.mobile
-            print ("mobilemobile",mobile)
-            if mobile:
-                data={
-                'message':sms_template_objs.get_body_data(self),
-                'mobile':mobile,
-                'model_id':self.id,
-                'partner_id':self.partner_id.id,
-                'model_name':'account.invoice',
-                }
+        try:
+            sms_template_objs = self.env["sms.template"].sudo().search(
+                    [('condition', '=', 'invoice_vaildate')])
+            print ("sms_template_objssms_template_objs",sms_template_objs)
+            for sms_template_obj in sms_template_objs:
+                mobile = self.partner_id.mobile
+                print ("mobilemobile",mobile)
+                if mobile:
+                    data={
+                    'message':sms_template_objs.get_body_data(self),
+                    'mobile':mobile,
+                    'model_id':self.id,
+                    'partner_id':self.partner_id.id,
+                    'model_name':'account.invoice',
+                    }
 
-                api_obj.makecall(data)
+                    api_obj.makecall(data)
+        except Exception:
+            continue
         return invoice_result
 
                    
